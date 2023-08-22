7df49cc775be2d8bf3c5f25a8843b19e4626f3ca
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-22 17:45:32 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

---
+++ plugins/descargas-pptiktok.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args, text, command, usedPrefix }) => {
+if (!text) throw `𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙙𝙚 𝙩𝙞𝙠𝙩𝙤𝙠
+𝙎𝙞𝙣 𝙪𝙨𝙖𝙧 "@"\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤\n*${usedPrefix + command} mundo_dos_animes81*`
+let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
+await conn.sendFile(m.chat, res, 'error.jpg', `✅ 𝙁𝙤𝙩𝙤 𝙙𝙚 𝙥𝙚𝙧𝙛𝙞𝙡*${text}*`, m, false)
+  
+}
+handler.help = ['tiktokfoto'].map(v => v + ' <username>')
+handler.tags = ['downloader']
+handler.command = /^(tiktokfoto|tiktokphoto)$/i
+handler.limit = 1
+handler.exp = 68
+export default handler
---
+++ plugins/fun-formarpareja.js
@@ -0,0 +1,18 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+let vn = './media/Vivan.mp3'
+conn.sendFile(m.chat, vn, 'Vivan.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
+m.reply(`*${toM(a)}, 𝙔𝙖 𝙚𝙨 𝙝𝙤𝙧𝙖 𝙙𝙚 𝙦𝙪𝙚 𝙩𝙚 💍 𝘾𝙖𝙨𝙚𝙨 𝙘𝙤𝙣 ${toM(b)}, 𝙇𝙞𝙣𝙙𝙖 𝙋𝙖𝙧𝙚𝙟𝙖 😉💓*`, null, {
+mentions: [a, b]
+  
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['formarpareja','formarparejas']
+handler.group = true
+export default handler
