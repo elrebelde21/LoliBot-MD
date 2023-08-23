0e1bbec59762fdfc9bfb9197e46ee8501f1f8fa8
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-22 21:33:12 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ COMMENT
 echo -e "\033[01;93mPreparando instalación...\n\033[0m"
 mp3_array=("install_automatic1.mp3" "install_automatic2.mp3" "install_automatic3.mp3" "install_automatic4.mp3" "install_automatic5.mp3" "install_automatic6.mp3" "install_automatic7.mp3" "install_automatic8.mp3" "install_automatic9.mp3" "install_automatic10.mp3" "install_automatic11.mp3" "install_automatic12.mp3")
 random_mp3=${mp3_array[$RANDOM % ${#mp3_array[@]}]}
-if wget -q https://github.com/elrebelde21/The-LoliBot-MD/raw/master/musicSH/"$random_mp3"; then
+if wget -q https://github.com/elrebelde21/The-LoliBot-MD-V2/raw/master/musicSH/"$random_mp3"; then
 echo -e "\033[01;32mDescarga exitosa. Reproduciendo sonido de intro...\n\033[0m"
 while true; do
 mpv --no-terminal "$random_mp3" &
