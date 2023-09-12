09fc5a3725c02fbb626fc7ed9c1969ca5d579d65
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-09-12 19:15:28 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- plugins/maker-ytcomment.js
+++ plugins/maker-ytcomment.js
@@ -1,14 +1,12 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw 'No Text';
+  if (!text) throw `${lenguajeGB['smsAvisoMG']()} Y EL TEXTO?`
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender),
-  }), 'error.png', '*¡¡𝙶𝚁𝙰𝙲𝙸𝙰𝚂 𝙿𝙾𝚁 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁!!*', m);
+  }), 'error.png', '*HAS COMENTADO EN YOUTUBE!!* ?', m);
 };
 handler.help = ['ytcomment <comment>'];
 handler.tags = ['maker'];
 handler.command = /^(ytcomment)$/i;
-handler.limit = true
-handler.register = true
 export default handler;
