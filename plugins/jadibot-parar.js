321cd776db3a273ccc7df24305c7490dac339f13
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2023-12-09 20:28:11 -0300

    Delete plugins directory

--- plugins/jadibot-parar.js
+++
@@ -1,9 +0,0 @@
-let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `Por qué no vas directamente con el numero del Bot`, m)
-else {
-await conn.reply(m.chat, `Usted ha cerrado session`, m)
-conn.ws.close()
-}}
-handler.command = /^(berhenti|stop|detener)$/i
-  
-export default handler
--- plugins/propietario(a)-autoadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw 'Ya eres admin del grupo mi creador 🫡'
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^admin.|atad|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
