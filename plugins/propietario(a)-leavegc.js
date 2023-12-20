51f5bb5c546ac0d46b23f3d4b22feb88e75fa5fe
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2023-12-19 22:28:14 -0300

    Delete plugins directory

--- plugins/propietario(a)-leavegc.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-await conn.reply(id, '*𝑬𝒍 𝒃𝒐𝒕  𝒂𝒃𝒂𝒏𝒅𝒐𝒏𝒂 𝒆𝒍 𝒈𝒓𝒖𝒑𝒐, 𝒄𝒉𝒂𝒖👋*') 
-await conn.groupLeave(id)}
-handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
-handler.rowner = true
-export default handler
