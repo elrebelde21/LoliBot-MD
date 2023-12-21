1b296deb6b4f1a69d9c41cc36b04b01f477a226d
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2023-12-21 12:03:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ global.prems = ['972529277314'],
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "+15125626098" //Ejemplo: +59309090909
-global.confirmCode = "+15125626098" 
+global.botNumberCode = "" //Ejemplo: +59309090909
+global.confirmCode = "" 
 
 //---------[ IDIOMA : LENGUAJE ]---------
 //es = Español         id = Bahasa Indonesia
