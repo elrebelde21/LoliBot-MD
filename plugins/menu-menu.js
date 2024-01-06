2af21c3f6f2728e34846e8021024bcc5abbb7a59
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-06 18:18:10 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -79,7 +79,7 @@ let menu = `『 ${wm} 』
 
 □ *Creador:* Mario
 □ *Numero del creador:* ${asistencia}
-□ *Numero del bot ofc:* {bot}
+□ *Numero del bot ofc:* ${bot}
 □ *Tiempos activos :* ${uptime}
 □ *Usuarios:* ${Object.keys(global.db.data.users).length}
 □ *Registrado:* ${rtotalreg} de ${totalreg} ${(conn.user.jid == global.conn.user.jid ? '' : `\n□ *Soy un sub bot del:* wa.me/${global.conn.user.jid.split`@`[0]}`) || ''}
