84531aeffc33b6cf2ab7df2010d63be1f370e094
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-06 17:28:38 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- plugins/game-adv-peliculas.js
+++ plugins/game-adv-peliculas.js
@@ -22,7 +22,7 @@ let caption = `
 conn.tekateki[id] = [
 await //conn.reply(m.chat, caption, m),
 conn.sendMessage(m.chat, { text: caption, contextInfo:{forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "body": `• ADIVINAN LA PELÍCULA CON EMOJIS •`, "previewType": "PHOTO", thumbnail: imagen1, sourceUrl: md}}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}), json, poin, setTimeout(async () => {
-if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
+if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!`, conn.tekateki[id][0])
 delete conn.tekateki[id]
 }, timeout)
 ]}
