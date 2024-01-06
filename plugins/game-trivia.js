5cfec949f7344261f16428f15a7f3fc75893fb26
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-06 16:41:28 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- plugins/game-trivia.js
+++ plugins/game-trivia.js
@@ -1,15 +1,15 @@
 import fs from 'fs'
-let timeout = 30000
+let timeout = 10000
 let poin = 500
 
 let handler = async (m, { conn, usedPrefix }) => {
 conn.tekateki = conn.tekateki ? conn.tekateki : {}
 let id = m.chat
 if (id in conn.tekateki) {
-conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
+conn.reply(m.chat, 'Todavía hay una pregunta sin responder en este chat', conn.tekateki[id][0])
 throw false
 }
-let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
+let tekateki = JSON.parse(fs.readFileSync(`./src/game/trivia.json`))
 let json = tekateki[Math.floor(Math.random() * tekateki.length)]
 let _clue = json.response
 let clue = _clue.replace(/[A-Za-z]/g, '_')
@@ -18,17 +18,19 @@ let caption = `
 
 *• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *• Bono:* +${poin} Exp
+
+💫 Responde a este mensaje con la letra de la opción correcta ✅
+¡Tienes 10 segundos!
 `.trim()
 conn.tekateki[id] = [
 await //conn.reply(m.chat, caption, m),
 conn.sendMessage(m.chat, { text: caption, contextInfo:{forwardingScore: 9999999, isForwarded: true, "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "body": `• 𝐀𝐂𝐄𝐍𝐓𝐈𝐉𝐎 •`, "previewType": "PHOTO", thumbnail: imagen1, sourceUrl: md}}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}), json, poin, setTimeout(async () => {
-if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
+if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!`, conn.tekateki[id][0])
 delete conn.tekateki[id]
 }, timeout)
 ]}
-handler.help = ['acertijo']
+handler.help = ['trivia']
 handler.tags = ['game']
-handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i
+handler.command = /^(trivia|triviador|adivinaza)$/i
 handler.register = true
-
 export default handler
