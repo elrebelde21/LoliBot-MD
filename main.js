285faaf3f7dbac7b00afb3aa7370fc0a9ca69a10
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-02-18 20:49:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['The-LoliBot-MD', 'Chrome', '2.0.0'] : methodCodeQR ? ['The-LoliBot-MD', 'Chrome', '2.0.0'] : ['Chrome (Linux)', '', ''],
+browser: opcion == '1' ? ['LoliBot-MD', 'Edge', '2.0.0'] : methodCodeQR ? ['LoliBot-MD', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
