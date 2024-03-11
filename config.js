4ce02ad08127c4f96741ab393d54497ea62a42c9
ZENKI21 <108560476+Zenki21@users.noreply.github.com>
2024-03-11 17:37:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.prems = ['972529277314'],
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "" //Ejemplo: +59309090909
+global.botNumberCode = "562961881" //Ejemplo: +59309090909
 global.confirmCode = "" 
 
 //---------[ IDIOMA : LENGUAJE ]---------
