da005f8f9c46326c078123af220a34333c6174fa
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-18 11:34:47 -0300

    Add files via upload

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
     
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 
-let noetiqueta = fs.readFileSync('https://qu.ax/MKCm.webp')
+let noetiqueta = 'https://qu.ax/MKCm.webp'
 let or = ['texto', 'sticker']; 
 let media = or[Math.floor(Math.random() * 2)]
 if (media === 'sticker') return await this.sendFile(m.chat, noetiqueta, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'Yo que?', mediaType: 2, sourceUrl: redes.getRandom(), thumbnail: img.getRandom()}}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
