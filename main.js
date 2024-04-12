9b839e9731e0121f9dbe9883ec70f0a74d94fa6e
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-04-12 17:29:39 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- main.js
+++ main.js
@@ -170,7 +170,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['LoliBot-MD', 'Edge', '2.0.0'] : methodCodeQR ? ['LoliBot-MD', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
+browser: opcion == '1' ? ['LoliBot-MD', 'Edge', '1.0.0'] : methodCodeQR ? ['LoliBot-MD', 'Edge', '1.0.0'] : ['Ubuntu', 'Edge', '2.0.0'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
