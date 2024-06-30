5d48eaf27e8b0739f5814fb62450fa07dd37645a
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-30 20:56:45 -0300

    Delete plugins directory

--- plugins/propietario(a)-cmd-list.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-conn.reply(m.chat, `*⪻ ʟɪsᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏ / ᴛᴇxᴛᴏs ᴀsɪɢɴᴀᴅᴏ ⪼"
-
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*ᴄᴏᴅɪɢᴏ:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*ᴄᴏᴍᴀɴᴅᴏ/ᴛᴇxᴛᴏs:* ${value.text}`).join('\n\n')}
-`.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
-}
-handler.command = ['listcmd', 'cmdlist']
-handler.rowner = true
-export default handler
