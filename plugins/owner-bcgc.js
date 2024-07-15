31bb93306dce79d93be8828cb1df251b1031dfbb
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-07-15 19:39:15 -0300

    Add files via upload

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -6,13 +6,13 @@ if (!text && !m.quoted) return m.reply('*⚠️ 𝐈𝐧𝐠𝐫𝐞𝐬𝐞 �
 let users = participants.map(u => conn.decodeJid(u.id))
 let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks2 = text ? text : cc2.text 
-let d = new Date(new Date + 3600000)
+//let d = new Date(new Date + 3600000)
 //let locale = lenguajeGB.lenguaje()
-let dia = d.toLocaleDateString(locale, { weekday: 'long' })
+//let dia = d.toLocaleDateString(locale, { weekday: 'long' })
 //let fecha = d.toLocaleDateString(lenguajeGB.lenguaje(), { day: 'numeric', month: 'numeric', year: 'numeric' })
 //let mes = d.toLocaleDateString(", { month: 'long' })
 //let año = d.toLocaleDateString(lenguajeGB.lenguaje(), { year: 'numeric' })
-let tiempo = d.toLocaleString('es-CO', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+//let tiempo = d.toLocaleString('es-CO', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 let groups = Object.keys(await conn.groupFetchAllParticipating())
 let usersTag = participants.map(u => conn.decodeJid(u.id))
 let readMS = String.fromCharCode(8206).repeat(850)
