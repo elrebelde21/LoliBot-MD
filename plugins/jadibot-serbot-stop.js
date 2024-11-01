aa2a40e765ad9cb0ce07bce62a119506f7be5a45
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-01 16:07:36 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- plugins/jadibot-serbot-stop.js
+++ plugins/jadibot-serbot-stop.js
@@ -2,6 +2,7 @@ let handler = async (m, { conn }) => {
 if (global.conn.user.jid === conn.user.jid) {
 } else {
 await conn.reply(m.chat, `Adios bot :(`, m)
+conn.fstop = true
 conn.ws.close()
 }}
 handler.help = ['stop']
