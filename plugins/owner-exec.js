3ffcd0369377c6e544830ed177833a09eed6ea0c
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-11-01 10:48:10 -0300

    Update owner-exec.js

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 
 let handler = async (m, _2, msg, ROwner, isOwner, pickRandom) => {
-if (!isOwner return;
+if (!isOwner) return;
 
 let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
