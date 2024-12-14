a7b4ed394f8073d01defb54ba900977f48ee6f62
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-12-14 12:52:52 -0300

    Add files via upload

--- install2.sh
+++ install2.sh
@@ -3,17 +3,19 @@
 # Detener la ejecución si ocurre un error
 set -e
 
-echo "Descargando node_modules.tar.gz..."
-curl -L -o node_modules.tar.gz https://github.com/elrebelde21/NovaBot_MD/releases/download/1.1.8/node_modules.tar.gz
+if [ -d "node_modules" ]; then
+    echo "La carpeta 'node_modules' ya existe. Omitiendo descarga e instalación."
+else
+    echo "Descargando node_modules.tar.gz..."
+    curl -L -o node_modules.tar.gz https://github.com/elrebelde21/NovaBot_MD/releases/download/1.1.8/node_modules.tar.gz
 
-echo "Extrayendo node_modules..."
-tar -xzf node_modules.tar.gz
+    echo "Extrayendo node_modules..."
+    tar -xzf node_modules.tar.gz
 
-# Eliminar el archivo comprimido después de extraerlo
-rm node_modules.tar.gz
+    # Eliminar el archivo comprimido después de extraerlo
+    rm node_modules.tar.gz
 
-echo "Verificando dependencias adicionales..."
-npm install
+fi
 
 echo "Iniciando el bot..."
 npm start
