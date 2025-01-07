eb43529fad6e842f83b23f43f76fe4414970fa75
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-07 19:23:43 -0300

    Add files via upload

--- handler.js
+++ handler.js
@@ -1060,6 +1060,8 @@ const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == this.use
 const isRAdmin = user?.admin == 'superadmin' || false
 const isAdmin = isRAdmin || user?.admin == 'admin' || false // Is User Admin?
 const isBotAdmin = bot?.admin || false // Are you Admin?
+m.isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
+m.isChannel = m.chat.includes('@newsletter') || m.sender.includes('@newsletter')
 
         const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins')
         for (let name in global.plugins) {
