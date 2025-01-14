d4d5d3f56fef3040e446def9f5933428491d572c
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-14 20:14:54 -0300

    Add files via upload

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,94 +1,65 @@
 import db from '../lib/database.js';
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import ws from 'ws'
+import ws from 'ws';
 import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os';
 import os from 'os';
 import moment from 'moment';
 import speed from 'performance-now';
 import { sizeFormatter } from 'human-readable';
-import si from 'systeminformation';
 
-let format = sizeFormatter({std: 'JEDEC',
-decimalPlaces: 2,
-keepTrailingZeroes: false,
-render: (literal, symbol) => `${literal} ${symbol}B`,
-});
-
-const __filename = fileURLToPath(import.meta?.url)
-const __dirname = path?.dirname(__filename)
-const carpetaBase = path?.resolve(__dirname, '..', 'jadibts')
-const cantidadCarpetas = (fs?.readdirSync(carpetaBase, { withFileTypes: true }).filter(item => item?.isDirectory())?.length) || 0
+let format = sizeFormatter({std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`,});
 
 const used = process.memoryUsage();
-    let ram = await si.mem()
-    let cpu = await si.cpuCurrentSpeed()
-    let disk = await si.fsSize()
-    let up = await si.time()
-      
+
 async function getSystemInfo() {
-let disk = await si.fsSize();
-const memInfo = await si.mem();
-const load = await si.currentLoad();
-    
-let cpuInfo = os.cpus();
-let modeloCPU = cpuInfo && cpuInfo.length > 0 ? cpuInfo[0].model : 'Modelo de CPU no disponible';
-let espacioTotalDisco = 'Información no disponible';
-if (disk && disk.length > 0) {
-espacioTotalDisco = humanFileSize(disk[0].available, true, 1) + ' libre de ' + humanFileSize(disk[0].size, true, 1);
-}
+    let cpuInfo = os.cpus();
+    let modeloCPU = cpuInfo && cpuInfo.length > 0 ? cpuInfo[0].model : 'Modelo de CPU no disponible';
+    let espacioTotalDisco = 'Información no disponible';
 
-const data = {
-latencia: 'No disponible',
-plataforma: os.platform(),
-núcleosCPU: cpuInfo ? cpuInfo.length : 'No disponible',
-modeloCPU: modeloCPU,
-arquitecturaSistema: os.arch(),
-versiónSistema: os.release(),
-procesosActivos: os.loadavg()[0],
-porcentajeCPUUsada: load.currentLoad.toFixed(2) + '%',
-memory: humanFileSize(ram.free, true, 1) + ' libre de ' + humanFileSize(ram.total, true, 1),
-ramUsada: (memInfo.used / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-ramTotal: (memInfo.total / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-ramLibre: (memInfo.free / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-porcentajeRAMUsada: ((memInfo.used / memInfo.total) * 100).toFixed(2) + '%',
-espacioTotalDisco: espacioTotalDisco,
-tiempoActividad: 'No disponible',
-cargaPromedio: os.loadavg().map((avg, index) => `${index + 1} min: ${avg.toFixed(2)}.`).join('\n'),
-horaActual: new Date().toLocaleString(),
-detallesCPUNúcleo: load.cpus.map(cpu => cpu.load.toFixed(2) + '%')
-};
+    const data = {
+        latencia: 'No disponible',
+        plataforma: os.platform(),
+        núcleosCPU: cpuInfo ? cpuInfo.length : 'No disponible',
+        modeloCPU: modeloCPU,
+        arquitecturaSistema: os.arch(),
+        versiónSistema: os.release(),
+        procesosActivos: os.loadavg()[0],
+        porcentajeCPUUsada: 'No disponible',
+        memory: humanFileSize(used.free, true, 1) + ' libre de ' + humanFileSize(used.total, true, 1),
+        ramUsada: 'No disponible',
+        ramTotal: 'No disponible',
+        ramLibre: 'No disponible',
+        porcentajeRAMUsada: 'No disponible',
+        espacioTotalDisco: espacioTotalDisco,
+        tiempoActividad: 'No disponible',
+        cargaPromedio: os.loadavg().map((avg, index) => `${index + 1} min: ${avg.toFixed(2)}.`).join('\n'),
+        horaActual: new Date().toLocaleString(),
+    };
 
-const startTime = Date.now();
-await si.currentLoad();
-const endTime = Date.now();
-data.latencia = `${endTime - startTime} ms`;
-const uptimeSeconds = await si.time().uptime;
-const days = Math.floor(uptimeSeconds / 60 / 60 / 24);
-const hours = Math.floor((uptimeSeconds / 60 / 60) % 24);
-const minutes = Math.floor((uptimeSeconds / 60) % 60);
+    const startTime = Date.now();
+    const endTime = Date.now();
+    data.latencia = `${endTime - startTime} ms`;
 
-data.tiempoActividad = `${days}d ${hours}h ${minutes}m`;
-return data;
+    return data;
 }
 
 let handler = async (m, { conn, usedPrefix }) => {
-let bot = global.db.data.settings[conn.user.jid];
-let _uptime = process.uptime() * 1000;
-let uptime = new Date(_uptime).toISOString().substr(11, 8);
-let totalreg = Object.keys(global.db.data.users).length;
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
-let totalbots = Object.keys(global.db.data.settings).length;
-let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0);
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats);
-let totalchats = Object.keys(global.db.data.chats).length;
-let totalf = Object.values(global.plugins).filter(v => v.help && v.tags).length;
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'));
-let totaljadibot = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-const totalUsers = totaljadibot.length;
-let timestamp = speed();
-let latensi = speed() - timestamp;
+    let bot = global.db.data.settings[conn.user.jid];
+    let _uptime = process.uptime() * 1000;
+    let uptime = new Date(_uptime).toISOString().substr(11, 8);
+    let totalreg = Object.keys(global.db.data.users).length;
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
+    let totalbots = Object.keys(global.db.data.settings).length;
+    let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0);
+    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats);
+    let totalchats = Object.keys(global.db.data.chats).length;
+    let totalf = Object.values(global.plugins).filter(v => v.help && v.tags).length;
+    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'));
+    let totaljadibot = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+    const totalUsers = totaljadibot.length;
+    let timestamp = speed();
+    let latensi = speed() - timestamp;
+    const { restrict } = global.db.data.settings[conn.user.jid] || {}
+    const { autoread } = global.opts    
 
 getSystemInfo().then(async (data) => {
 let teks = `*≡ INFOBOT*
@@ -114,12 +85,11 @@ let teks = `*≡ INFOBOT*
 ▣ *Núcleos de CPU:* ${data.núcleosCPU} 
 ▣ *CPU Usada:* ${data.porcentajeCPUUsada} 
 ▣ *Ram usada:* ${format(totalmem() - freemem())} / ${format(totalmem())}
-▣ *Espacio Total en Disco:* ${data.espacioTotalDisco} 
-▣ *Uptime:* ${data.tiempoActividad}`;
+▣ *Uptime:* ${toTime(os.uptime() * 1000)}`;
 
 await conn.sendMessage(m.chat, {image: { url: "https://telegra.ph/file/39fb047cdf23c790e0146.jpg" }, caption: teks, contextInfo: {externalAdReply: { title: `INFO - BOT`, sourceUrl: redes.getRandom(), mediaType: 1, showAdAttribution: true, thumbnailUrl: img1,
 }}}, { quoted: m })});
-};
+}
 handler.help = ['infobot'];
 handler.tags = ['main'];
 handler.command = /^(infobot|informacionbot|infololi)$/i;
@@ -127,20 +97,29 @@ handler.register = true;
 export default handler;
 
 function toNum(number) {
-if (number >= 1000 && number < 1000000) {
-return (number / 1000).toFixed(1) + 'k';
-} else if (number >= 1000000) {
-return (number / 1000000).toFixed(1) + 'M';
-} else if (number <= -1000 && number > -1000000) {
-return (number / 1000).toFixed(1) + 'k';
-} else if (number <= -1000000) {
-return (number / 1000000).toFixed(1) + 'M';
-} else {
-return number.toString();
-}}
+    if (number >= 1000 && number < 1000000) {
+        return (number / 1000).toFixed(1) + 'k';
+    } else if (number >= 1000000) {
+        return (number / 1000000).toFixed(1) + 'M';
+    } else if (number <= -1000 && number > -1000000) {
+        return (number / 1000).toFixed(1) + 'k';
+    } else if (number <= -1000000) {
+        return (number / 1000000).toFixed(1) + 'M';
+    } else {
+        return number.toString();
+    }
+}
 
 function humanFileSize(bytes) {
-const unidades = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
-const exponente = Math.floor(Math.log(bytes) / Math.log(1024));
-return `${(bytes / Math.pow(1024, exponente)).toFixed(2)} ${unidades[exponente]}`;
-    }
+    const unidades = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    const exponente = Math.floor(Math.log(bytes) / Math.log(1024));
+    return `${(bytes / Math.pow(1024, exponente)).toFixed(2)} ${unidades[exponente]}`;
+}
+
+function toTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes, ${seconds % 60} seconds`;
+}
--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -1,72 +1,21 @@
 import { cpus as _cpus, totalmem, freemem } from 'os'
 import util from 'util'
 import os from 'os'
-import moment from 'moment';
 import fetch from 'node-fetch'
 import osu from 'node-os-utils'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
-import si from 'systeminformation';
-
 let format = sizeFormatter({
 std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
 decimalPlaces: 2,
 keepTrailingZeroes: false,
 render: (literal, symbol) => `${literal} ${symbol}B`,
 })
-
-async function getSystemInfo() {
-let disk = await si.fsSize();
-const memInfo = await si.mem();
-const load = await si.currentLoad();
-    
-let cpuInfo = os.cpus();
-let modeloCPU = cpuInfo && cpuInfo.length > 0 ? cpuInfo[0].model : 'Modelo de CPU no disponible';
-let espacioTotalDisco = 'Información no disponible';
-if (disk && disk.length > 0) {
-espacioTotalDisco = humanFileSize(disk[0].available, true, 1) + ' libre de ' + humanFileSize(disk[0].size, true, 1);
-}
-
-const data = {
-latencia: 'No disponible',
-plataforma: os.platform(),
-núcleosCPU: cpuInfo ? cpuInfo.length : 'No disponible',
-modeloCPU: modeloCPU,
-arquitecturaSistema: os.arch(),
-versiónSistema: os.release(),
-procesosActivos: os.loadavg()[0],
-porcentajeCPUUsada: load.currentLoad.toFixed(2) + '%',
-//memory: humanFileSize(ram.free, true, 1) + ' libre de ' + humanFileSize(ram.total, true, 1),
-ramUsada: (memInfo.used / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-ramTotal: (memInfo.total / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-ramLibre: (memInfo.free / (1024 * 1024 * 1024)).toFixed(2) + ' GB',
-porcentajeRAMUsada: ((memInfo.used / memInfo.total) * 100).toFixed(2) + '%',
-espacioTotalDisco: espacioTotalDisco,
-tiempoActividad: 'No disponible',
-cargaPromedio: os.loadavg().map((avg, index) => `${index + 1} min: ${avg.toFixed(2)}.`).join('\n'),
-horaActual: new Date().toLocaleString(),
-detallesCPUNúcleo: load.cpus.map(cpu => cpu.load.toFixed(2) + '%')
-};
-
-//Calcula la latencia (tiempo de respuesta)
-const startTime = Date.now();
-await si.currentLoad();
-const endTime = Date.now();
-data.latencia = `${endTime - startTime} ms`;
-
-//Calcula el tiempo de actividad
-const uptimeSeconds = await si.time().uptime;
-const days = Math.floor(uptimeSeconds / 60 / 60 / 24);
-const hours = Math.floor((uptimeSeconds / 60 / 60) % 24);
-const minutes = Math.floor((uptimeSeconds / 60) % 60);
-
-data.tiempoActividad = `${days}d ${hours}h ${minutes}m`;
-return data;
-}
-
 let handler = async (m, { conn, usedPrefix, command, isRowner}) => {
 try {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+//let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🐈', body: 'support group' , sourceUrl: accountsgb, thumbnailUrl: await(await fetch(gataMenu)).buffer() }}}
+//let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(gataMenu)).buffer(), sourceUrl: accountsgb }}}
+//let dos = [enlace, enlace2]
 
 let _muptime
 if (process.send) {
@@ -108,12 +57,9 @@ irq: 0
 
 let old = performance.now()
 const { key } = await conn.sendMessage(m.chat, {text: `𝘾𝙤𝙢𝙚𝙣𝙯𝙖𝙣𝙙𝙤 𝙥𝙧𝙪𝙚𝙗𝙖 𝙙𝙚𝙡 𝙫𝙚𝙡𝙤𝙘𝙞𝙙𝙖𝙙 🚀...`}, {quoted: fkontak});
-// await delay(1000 * 2);
-//await conn.reply(m.chat, `𝘾𝙤𝙢𝙚𝙣𝙯𝙖𝙣𝙙𝙤 𝙥𝙧𝙪𝙚𝙗𝙖 𝙙𝙚𝙡 𝙫𝙚𝙡𝙤𝙘𝙞𝙙𝙖𝙙 🚀...`, fkontak,  m)
+
 let neww = performance.now()
 let speed = neww - old
-
-getSystemInfo().then(async (data) => {
 let caption = `*🚀 Velocidad 🚀*
 
 🚄 *${Math.round(neww - old)}* ms
@@ -130,20 +76,11 @@ ${readMore}
 ▢ *${chats.length}* _Total Chats_
 
 *🔰 S E R V I D O R : S E R V E R*
-*💻 Plataforma :* \`\`\`${os.platform()}\`\`\`
-*📡 Servidor :* _${os.hostname()}_
-*🔢 Núcleos de CPU :* _${data.núcleosCPU}_
-*📡 Modelo de CPU :* _${data.modeloCPU}_
-*🏗️ Arquitectura del Sistema :* _${data.arquitecturaSistema}_
-*🔢 Versión del Sistema :* _${data.versiónSistema}_
-*📈 Procesos Activos :* _${data.procesosActivos}_
-*🔳 CPU Usada :* _${data.porcentajeCPUUsada}_
 *🟢 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
 *🔵 RAM Libre:* ${format(freemem())}
-*💾 Porcentaje de RAM Usada :* _${data.porcentajeRAMUsada}_
-*📦 Espacio Total en Disco :* _${data.espacioTotalDisco}_
-*⏳ Uptime :* _${data.tiempoActividad}_
-
+*💻 Plataforma :* \`\`\`${os.platform()}\`\`\`
+*📡 Servidor :* _${os.hostname()}_
+*⏳ Uptime :* _${toTime(os.uptime() * 1000)}_
 ${readMore}
 *NodeJS Uso de memoria : Memory Usage*
 ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
@@ -152,11 +89,8 @@ ${cpus[0] ? `*Uso total de la CPU*
 ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
 
 *CPU Core(s) Uso (${cpus.length} Core CPU)*
-${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
-`
-await conn.sendMessage(m.chat, {text: caption, edit: key})});
-//await conn.sendFile(m.chat, gataImg.getRandom(), 'gata.jpg', caption, fkontak)
-///await conn.sendButton(m.chat, wm, caption, gata.getRandom(), [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, dos.getRandom())
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}`
+await conn.sendMessage(m.chat, {text: caption, edit: key});
 } catch (e) {
 await conn.reply(m.chat, `${fg}*ALGO SALIÓ MAL.*\n\n\`\`\`REPORTE ESTE COMANDO ${usedPrefix + command} CON EL COMANDO ${usedPrefix}reporte\`\`\``, m)
 console.log(e) 
@@ -175,11 +109,13 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [' ' + d, ' dias, ', h, ' hs, ', m, ' min, ', s, ' seg '].map(v => v.toString().padStart(2, 0)).join('')
+  return [' ' + d, ' *Días : Days ☀️*\n ', h, ' *Horas : Hours 🕐*\n ', m, ' *Minuto : Minute ⏰*\n ', s, ' *Segundo : Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
 
-function humanFileSize(bytes) {
-    const unidades = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
-    const exponente = Math.floor(Math.log(bytes) / Math.log(1024));
-    return `${(bytes / Math.pow(1024, exponente)).toFixed(2)} ${unidades[exponente]}`;
+function toTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes, ${seconds % 60} seconds`;
 }
