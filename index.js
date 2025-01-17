e12ca3cb5a41a5016c362af01162734377e56d89
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-17 18:00:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 console.log('Iniciando 🚀🚀🚀') 
 import { join, dirname } from 'path'
-import { createRequire } from 'module'
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts'
+import cfonts from 'cfonts';
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import diskusage from 'diskusage';  
+import express from 'express'
+import chalk from 'chalk'
+import path from 'path'
+import os from 'os'
+import { promises as fsPromises } from 'fs'
+
+// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-const { name, author } = require(join(__dirname, './package.json'))
+const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
- 
+
 say('LoliBot-MD', {
 font: 'chrome',
 align: 'center',
@@ -23,68 +29,57 @@ font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
 
-let isRunning = false
+var isRunning = false
 
-function checkDiskUsage() {
-const path = process.cwd(); 
-diskusage.check(path, (err, info) => {
-if (err) {
-console.error(err);
-return;
-}
-
-const limit = 10 * 1024 * 1024 * 1024; //10Gbs
-if (info.free < limit) {
-console.log('Se ha superado el límite de 10 GB, reiniciando el bot...');
-process.emit('SIGINT'); //restart 
-}
-});
-}
-
-/**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
-function start(file) {
-if (isRunning) return
-isRunning = true
-const args = [join(__dirname, file), ...process.argv.slice(2)]
-
-setupMaster({
-exec: args[0],
-args: args.slice(1)})
-const p = fork()
-p.on('message', (data) => {
+async function start(file) {
+  if (isRunning) return
+  isRunning = true
+  const currentFilePath = new URL(import.meta.url).pathname
+  let args = [join(__dirname, file), ...process.argv.slice(2)]
+  say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1),
+  })
+  let p = fork()
+  p.on('message', (data) => {
 //console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
-case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
-case 'uptime':
-p.send(process.uptime())
-break
-}
-})
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('⚠️ Error Inesperado ⚠️', code)
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
-}})
-const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test']) {
-if (!rl.listenerCount()) {
-rl.on('line', (line) => {
-p.emit('message', line.trim())
-})
-}}}
-start('main.js')
+      case 'reset':
+        p.process.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  //---
+  p.on('exit', (_, code) => {
+    isRunning = false
+    console.error('⚠️ Error Inesperado ⚠️', code)
+    start('main.js'); //
 
-setInterval(checkDiskUsage, 600000); //10 minutos
+    if (code === 0) return
+    watchFile(args[0], () => {
+      unwatchFile(args[0])
+      start(file)
+    })
+  })
+
+  setInterval(() => {}, 1000);
+
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  if (!opts['test'])
+    if (!rl.listenerCount()) rl.on('line', line => {
+      p.emit('message', line.trim())
+    })
+  // console.log(p)
+}
+
+start('main.js')
