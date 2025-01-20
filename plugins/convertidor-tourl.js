01e359cda79e315973fbf3da863c95c3ed79a12f
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-20 18:47:01 -0300

    Add files via upload

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,14 +1,22 @@
 import uploadFile from '../lib/uploadFile.js';
+import upload from '../lib/uploadFile2.js';
 import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
 const q = m.quoted ? m.quoted : m;
 const mime = (q.msg || q).mimetype || '';
 if (!mime) throw '*⚠️ ¿𝐘 𝐋𝐀 𝐈𝐌𝐀𝐆𝐄𝐍? 𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐚 𝐮𝐧𝐚 𝐢𝐦𝐚𝐠𝐞𝐧 𝐨 𝐯𝐢𝐝𝐞𝐨 𝐞𝐥 𝐜𝐮𝐚𝐥 𝐬𝐞𝐫𝐚 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐢𝐝𝐨 𝐚 𝐞𝐧𝐥𝐚𝐜𝐞*';
 const media = await q.download();
+try {
+const link = await upload(media);
+m.reply(link);
+} catch (e) {
+try {    
 const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
 const link = await (isTele ? uploadImage : uploadFile)(media);
 m.reply(link);
-};
+} catch (e) {
+console.log(e) 
+}}}
 handler.help = ['tourl <reply image>'];
 handler.tags = ['convertidor']
 handler.command = /^(upload|tourl)$/i;
