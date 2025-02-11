d05df2ee2f778e4c6fc7613d9d82e2a6a7a494ab
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-11 14:49:21 -0300

    Add files via upload

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,50 +1,37 @@
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-
-let setting = global.db.data.settings[this.user.jid]
-const settingsREAD = global.db.data.settings[this.user.jid] || {}
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+let setting = global.db.data.settings[this.user.jid];
+const settingsREAD = global.db.data.settings[this.user.jid] || {};
 
 if (m.text && prefixRegex.test(m.text)) {
-this.sendPresenceUpdate('composing', m.chat)
-this.readMessages([m.key])
-        
-let usedPrefix = m.text.match(prefixRegex)[0]
-let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
+this.sendPresenceUpdate('composing', m.chat);  
+this.readMessages([m.key]);  
+    
+let usedPrefix = m.text.match(prefixRegex)[0];
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase(); 
 }
 
 if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = {};
-if (!global.db.data.users[m.sender].mensaje) global.db.data.users[m.sender].mensaje = {};
-if (!global.db.data.users[m.sender].mensaje[m.chat]) global.db.data.users[m.sender].mensaje[m.chat] = 0;
-global.db.data.users[m.sender].mensaje[m.chat]++;
+  if (!global.db.data.users[m.sender].mensaje) global.db.data.users[m.sender].mensaje = {};
+  if (!global.db.data.users[m.sender].mensaje[m.chat]) global.db.data.users[m.sender].mensaje[m.chat] = 0;
+  global.db.data.users[m.sender].mensaje[m.chat]++;
 
-if (m.fromMe) return
-if (m.isGroup) return !1
-if (!m.message) return !0 
-if (m.chat === "120363297379773397@newsletter") return; 
-if (m.chat === "120363355261011910@newsletter") return;
-//if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') || m.text.includes('estado') || m.text.includes('code') || m.text.includes('Code') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('reg') || m.text.includes('verificar') || m.text.includes('Serbot') || m.text.includes('Jadibot') || m.text.includes('jadibot code') || m.text.includes('serbot code') || m.text.includes('jadibot --code') || m.text.includes('serbot --code') || m.text.includes('code')) return !0
-const comandosPermitidos = ['PIEDRA', 'PAPEL', 'TIJERA', 'menu', 'estado', 'code', 'Code', 'bots',
-  'serbot', 'jadibot', 'reg', 'verificar', 'Serbot', 'Jadibot',
-  'jadibot code', 'serbot code', 'jadibot --code', 'serbot --code'];
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
+if (m.fromMe) return;
+if (m.isGroup) return !1;
+if (!m.message) return !0;
+let chat = global.db.data.chats[m.chat];
+let bot = global.db.data.settings[this.user.jid] || {};
 let user = global.db.data.users[m.sender] || {};
-/*if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`Hola esta prohibido usar los comando al privado del bot, por lo cual seras bloqueado...\n\n> _*Para usar mi funciones unirte al  grupo oficial 👇*_\n${[nnn, nnnttt, nnnt].getRandom()}`, false, { mentions: [m.sender] })
-await this.updateBlockStatus(m.chat, 'block')
-}*/
+
 if (bot.antiPrivate) {
-  if (!/^jadibot$/i.test(m.text.trim())) {
-    if (!user.warnPriv) {
-      await m.reply(`⚠️ *Atención* ⚠️\n\nEl bot no responde en privado. Solo puedes usar el comando *jadibot*. Para más funciones, únete al grupo oficial.`);
-      user.warnPriv = true; 
-      global.db.data.users[m.sender] = user;
+if (!m.chat.endsWith('g.us') && command !== 'jadibot') {
+if (!user.warnPriv) {
+await m.reply("⚠️ *Atención* ⚠️\n\nEl bot solo responde al comando *jadibot* en privado. Por favor, usa el comando *jadibot* para interactuar. Únete al grupo oficial para más funciones.");
+        user.warnPriv = true;       
+      }
+      return; 
     }
-    return; 
+    
+    return !1;
   }
 }
-
-return !1;
-}
-
-
