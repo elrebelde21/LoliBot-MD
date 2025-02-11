104609b2680df62d0a4b129ac84394fa817fe066
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-11 15:22:14 -0300

    Add files via upload

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,39 +1,39 @@
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+    let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
-let setting = global.db.data.settings[this.user.jid]
-const settingsREAD = global.db.data.settings[this.user.jid] || {}
+    let setting = global.db.data.settings[this.user.jid];
+    const settingsREAD = global.db.data.settings[this.user.jid] || {};
 
-if (m.text && prefixRegex.test(m.text)) {
-this.sendPresenceUpdate('composing', m.chat)
-this.readMessages([m.key])
+    if (m.text && prefixRegex.test(m.text)) {
+        this.sendPresenceUpdate('composing', m.chat);
+        this.readMessages([m.key]);
         
-let usedPrefix = m.text.match(prefixRegex)[0]
-let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
-}
+        let usedPrefix = m.text.match(prefixRegex)[0];
+        let command = m.text.slice(usedPrefix.length).trim().split(' ')[0];
+    }
 
-if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = {};
-if (!global.db.data.users[m.sender].mensaje) global.db.data.users[m.sender].mensaje = {};
-if (!global.db.data.users[m.sender].mensaje[m.chat]) global.db.data.users[m.sender].mensaje[m.chat] = 0;
-global.db.data.users[m.sender].mensaje[m.chat]++;
+    if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = {};
+    if (!global.db.data.users[m.sender].mensaje) global.db.data.users[m.sender].mensaje = {};
+    if (!global.db.data.users[m.sender].mensaje[m.chat]) global.db.data.users[m.sender].mensaje[m.chat] = 0;
+    global.db.data.users[m.sender].mensaje[m.chat]++;
 
-if (m.fromMe) return
+    if (m.fromMe) return
 if (m.isGroup) return !1
 if (!m.message) return !0 
-if (m.chat === "120363297379773397@newsletter") return; 
-if (m.chat === "120363355261011910@newsletter") return;
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') || m.text.includes('estado') || m.text.includes('code') || m.text.includes('Code') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('reg') || m.text.includes('verificar') || m.text.includes('Serbot') || m.text.includes('Jadibot') || m.text.includes('jadibot code') || m.text.includes('serbot code') || m.text.includes('jadibot --code') || m.text.includes('serbot --code') || m.text.includes('code')) return !0
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
-let user = global.db.data.users[m.sender] || {};
-if (bot.antiPrivate) {
-if (opts['gconly'] && !m.chat.endsWith('g.us')) return
-if (!user.warnPriv) {
-await m.reply(`Hola esta prohibido usar los comando al privado del bot, por lo cual seras bloqueado...\n\n> _*Para usar mi funciones unirte al  grupo oficial 👇*_\n${[nnn, nnnttt, nnnt].getRandom()}`, false, { mentions: [m.sender] })
-user.warnPriv = true;
-return
+
+    let chat = global.db.data.chats[m.chat];
+    let bot = global.db.data.settings[this.user.jid] || {};
+
+    if (bot.antiPrivate) {
+        if (m.text && prefixRegex.test(m.text)) {
+            const usedPrefix = m.text.match(prefixRegex)[0];
+            const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
+            
+if (command !== 'jadibot') {
+return true; 
 }
-//await this.updateBlockStatus(m.chat, 'block')
-}
-return !1
+        }
+    }
+    
+    return false;
 }
