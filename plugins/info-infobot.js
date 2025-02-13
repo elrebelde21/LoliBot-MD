5af557a2878725ec47e1b2a1eed0797e5b2a3a37
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-13 15:56:26 -0300

    Add files via upload

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -116,9 +116,14 @@ export default handler;
 async function getDiskUsage() {
     try {
         const path = "/home/container"; 
-        const usado = execSync(`du -sh ${path} | cut -f1`).toString().trim();
-        return { total: 'N/A', usado, libre: 'N/A' };
-    } catch (err) {
+        const { total, free } = diskusage.checkSync(path);
+        const used = total - free; 
+        return {
+            total: humanFileSize(total),
+            usado: humanFileSize(used),
+            libre: humanFileSize(free)
+        };
+    } catch (err) {     
         return { total: 'N/A', usado: 'N/A', libre: 'N/A' };
     }
 }
