fc4ed3c2399db373bc0fbcdbe1f8f77bfa969d5a
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-18 11:22:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 console.log('Iniciando 🚀🚀🚀') 
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
-
-// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname)
+const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
-
-//const app = express()
-//const port = process.env.PORT || 8080;
-
+ 
 say('LoliBot-MD', {
 font: 'chrome',
 align: 'center',
@@ -32,36 +22,20 @@ font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
 
-var isRunning = false
-
-process.on('uncaughtException', (err) => {
-if (err.code === 'ENOSPC') {
-console.error('Se ha detectado ENOSPC (sin espacio o límite de watchers alcanzado), reiniciando....')
-} else {
-console.error('Error no capturado:', err)
-}
-process.exit(1)
-})
-
-async function start(file) {
+let isRunning = false
+/**
+* Start a js file
+* @param {String} file `path/to/file`
+*/
+function start(file) {
 if (isRunning) return
 isRunning = true
-const currentFilePath = new URL(import.meta.url).pathname
-let args = [join(__dirname, file), ...process.argv.slice(2)]
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']
-})
-setupMaster({exec: args[0], args: args.slice(1),
-})
-let p = fork()
+const args = [join(__dirname, file), ...process.argv.slice(2)]
 
-setTimeout(() => {
-console.log('Reiniciando....');
-p.process.kill(); 
-}, 4 * 60 * 60 * 1000); //4hs
-  
+setupMaster({
+exec: args[0],
+args: args.slice(1)})
+const p = fork()
 p.on('message', (data) => {
 //console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
@@ -73,25 +47,32 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}})
-
+}
+})
 p.on('exit', (_, code) => {
+isRunning = false;
+console.error('⚠️ Error Inesperado ⚠️', code)
+p.process.kill()
 isRunning = false
-console.error('⚠️ Error Inesperado ⚠️ ', code)
-start('main.js'); //
-
-if (code === 0) return
-watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})})
-
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
+start.apply(this, arguments)
+if (process.env.pm_id) {
+process.exit(1)
+} else {
+process.exit()
+}})
+const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test']) {
+if (!rl.listenerCount()) {
+rl.on('line', (line) => {
 p.emit('message', line.trim())
-})}
+})
+}}}
 
-start('main.js')
+setInterval(() => {
+console.log('♻️ Reiniciando bot automáticamente...');
+process.exit(0); 
+}, 14400000) //4hs
+
+start('main.js');
 
 
