8595e01c27a9841c241b28deb7b741d03741dd19
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-24 07:24:51 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩

--- plugins/_antiArabe.js
+++ plugins/_antiArabe.js
@@ -1,7 +1,7 @@
 let handler = m => m;
 
 handler.before = async function (m, { conn }) {
-const prefijosProhibidos = ['91', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '212'];
+const prefijosProhibidos = ['91', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '249', '212'];
 const bot = global.db.data.settings[conn.user.jid] || {};
 const senderNumber = m.sender.split('@')[0];
 const user = global.db.data.users[m.sender]
