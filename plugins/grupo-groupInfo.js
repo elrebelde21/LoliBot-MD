d2fa4c6e16da6b1d84a9e729c68996cd96d3a2d3
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-02-27 19:31:31 -0300

    Add files via upload

--- plugins/grupo-groupInfo.js
+++ plugins/grupo-groupInfo.js
@@ -1,10 +1,51 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
-const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat]
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || imagen
+const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, antifake, antiTiktok, antiYoutube, antiTelegram, modoadmin, antiFacebook, antiInstagram, antiTwitter, antiDiscord, antiTwitch, antiThreads, delete: del } = global.db.data.chats[m.chat]
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-let text = `『 ＩＮＦＯ ＤＥ ＧＲＵＰＯ 』\n\n*• ID :*\n${groupMetadata.id}\n\n*• Nombre :*\n${groupMetadata.subject}\n\n*• Miembros :*\n${participants.length}\n\n*• Creador del. grupo :*\n@${owner.split('@')[0]}\n\n*• Admins :*\n${listAdmin}\n\n*• 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙽𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 :*\n• Bienvenida: ${welcome ? '✅' : '❌'}\n• Baneado: ${isBanned ? '✅' : '❌'}\n• Detect: ${detect ? '✅' : '❌'}\n• Anti eliminar: ${del ? '✅' : '❌'}\ • Antilink: ${antiLink ? '✅' : '❌'}\n\n*• 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 :*\n• Bienvenida: ${sWelcome}\n• Despedida: ${sBye}\n• Promovidos: ${sPromote}\n• Degradados: ${sDemote}\n\n*• 𝙳𝙴𝚂𝙲𝚁𝙸𝙲𝙸𝙾𝙽 :*\n${groupMetadata.desc?.toString() || 'desconocido'}`.trim()
+let socialMediaConfig = ''
+const socialMedia = [
+  { name: 'Tiktok', value: antiTiktok },
+  { name: 'Youtube', value: antiYoutube },
+  { name: 'Telegram', value: antiTelegram },
+  { name: 'Fb', value: antiFacebook },
+  { name: 'Ig', value: antiInstagram },
+  { name: 'Twitter (x)', value: antiTwitter },
+  { name: 'Discord', value: antiDiscord },
+  { name: 'Twitch', value: antiTwitch },
+  { name: 'Threads', value: antiThreads }
+]
+
+const activeSocialMedia = socialMedia.filter(sm => sm.value)
+if (activeSocialMedia.length > 0) {
+socialMediaConfig = activeSocialMedia.map(sm => `• Anti ${sm.name}: ✅`).join('\n')
+}
+
+let text = `『 ＩＮＦＯ ＤＥ ＧＲＵＰＯ 』
+
+*• ID :* 
+${groupMetadata.id}
+
+*• Nombre :* 
+${groupMetadata.subject}
+
+*• Miembros :*
+${participants.length}
+
+*• Creador del grupo :*
+@${owner.split('@')[0]}
+
+*• Admins :*
+${listAdmin} 
+
+*• 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙽𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 :*
+• Bot : ${modoadmin ? 'Apagado 📴' : 'Online ✅'}
+• Bienvenida: ${welcome ? '✅' : '❌'}
+• AntiLink: ${antiLink ? '✅' : '❌'}
+• AntiFake: ${antifake ? '✅' : '❌'}
+• Event: ${detect ? '✅' : '❌'}
+• Anti eliminar: ${del ? '✅' : '❌'} ${socialMediaConfig ? '\n' + socialMediaConfig : ''}`.trim()
 conn.sendFile(m.chat, pp, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['infogp']
