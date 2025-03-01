8cb8adafef5a656771236443a58790da4df8483e
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-03-01 15:43:49 -0300

    Add files via upload

--- handler.js
+++ handler.js
@@ -252,6 +252,7 @@ await delay(time)
 }, time)
 }
 
+const chat = global.db.data.chats[m.chat]
 //if(m.id.startsWith('NJX-') || m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20 || m.id.startsWith('FizzxyTheGreat-')) return
 if(m.id.startsWith('NJX-') || m.id.startsWith('Lyru-') || m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20 || m.id.startsWith('FizzxyTheGreat-')) return
 if (m.isGroup && chat.primaryBot && chat.primaryBot !== conn.user.jid) {
