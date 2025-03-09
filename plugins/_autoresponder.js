59e28dc4b586cf49317d145b9d2bec7d83a0e23c
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-03-09 20:53:33 -0300

    Add files via upload

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -14,6 +14,7 @@ let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=
 //if (prefixRegex.test(m.text)) return true;
 if (m.mentionedJid.includes(this.user.jid)) {
 //if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid)) {
+if (chat.simi) return;
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio') || m.text.includes('Bot') || m.text.includes('bot') || m.text.includes('Exp') || m.text.includes('diamante') || m.text.includes('lolicoins') || m.text.includes('Diamante') || m.text.includes('Lolicoins')) return !0
 if (["120363297379773397@newsletter", "120363355261011910@newsletter"].includes(m.chat)) return;
 await this.sendPresenceUpdate('composing', m.chat);
--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import { perplexity } from '../lib/chatgpt.js';
+const antiSpam = new Map();
 const handler = (m) => m;
 
 handler.before = async (m) => {
@@ -6,13 +8,61 @@ const chat = global.db.data.chats[m.chat];
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
 let textodem = m.text;
-if (m.text.includes('serbot') || m.text.includes('bots')|| m.text.includes('jadibot')|| m.text.includes('menu')|| m.text.includes('play')|| m.text.includes('play2') || m.text.includes('playdoc') || m.text.includes('tiktok') || m.text.includes('facebook') || m.text.includes('menu2') ||  m.text.includes('infobot') || m.text.includes('estado') ||  m.text.includes('ping') ||  m.text.includes('instalarbot') ||  m.text.includes('sc') || m.text.includes('sticker') ||  m.text.includes('s') || m.text.includes('wm') ||  m.text.includes('qc')) return
-if (m.fromMe) return
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+if (prefixRegex.test(m.text)) return true;
+const lastMessageTime = antiSpam.get(m.sender) || 0;
+const currentTime = Date.now();
+if (currentTime - lastMessageTime < 5000) return; 
+if (m.fromMe) return;
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
-let gpt = await fetch(`${apis}/tools/simi?text=${encodeURIComponent(textodem)}`)
-let res = await gpt.json() 
-await m.reply(res.data.message) 
+
+async function perplexityIA(q, logic) {
+try {
+let response = await perplexity.chat([{ role: 'system', content: logic || syms1 }, { role: 'user', content: q }], 'sonar-pro');
+if (response.status) {
+return response.result.response;
+} else {
+throw new Error(`Error en Perplexity: ${response.result.error}`);
+}} catch (error) {
+console.error('Error en Perplexity:', error);
+return null;
+}}
+    
+async function SimSimi(textodem, language = 'es') {
+try {
+const { data } = await axios.post("https://api.simsimi.vn/v1/simtalk", new URLSearchParams({
+textodem,
+lc: language }).toString(), {
+headers: {'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' }});
+return data.message;
+} catch (error) {
+console.error(error);
+return null;
+}}
+    
+let username = `${m.pushName}`;
+let syms1 = await fetch('https://raw.githubusercontent.com/elrebelde21/LoliBot-MD/main/src/text-chatgpt.txt').then(v => v.text());
+    
+let result;
+if (!result || result.trim().length === 0) {
+result = await perplexityIA(textodem, syms1);
+}
+
+if (!result || result.trim().length === 0) {
+result = await SimSimi(textodem);
+}
+    
+if (result && result.trim().length > 0) {
+await m.reply(result);
+antiSpam.set(m.sender, currentTime);
+}} catch {
+try {
+let gpt = await fetch(`${apis}/tools/simi?text=${textodem}`);
+let res = await gpt.json();
+await m.reply(res.data.message);
+antiSpam.set(m.sender, Date.now());
+antiSpam.set(m.sender, Date.now());
 } catch {
 /*SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA*/
 if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
@@ -25,9 +75,11 @@ const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn
 const res = await api.json();
 const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
 const resu2 = await reis2.json()
-await m.reply(resu2[0][0][0])}
-return !0;
+await m.reply(resu2[0][0][0])
+antiSpam.set(m.sender, Date.now());
 }
+return !0;
+}}
 return true;
 };
 export default handler;
