4730a7f66a19a5da7db40d074c62a216775f01f2
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-03-19 23:12:47 -0300

    Add files via upload

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no est�� registrado en la base de datos!*')
   let chat = global.db.data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
+  if (!chat.isBanned) return m.reply('*Este chat no est�� baneado!!*')
 chat.isBanned = false
-conn.reply(m.chat,  '*BOT ONLINE 🚀*', m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: ag, body: '𝐂𝐡𝐚𝐭 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐝𝐨', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}}) 
+conn.reply(m.chat,  '*BOT ONLINE ?*', m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: ag, body: '???? ??????????', previewType: 0, thumbnail: imagen4, sourceUrl: [md, yt, tiktok].getRandom()}}}) 
 }
 handler.command = /^unbanchat$/i
 //handler.botAdmin = true
-handler.rowner = true
+handler.owner = true
 
 export default handler
