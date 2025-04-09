f30600a1961c883f88dca09a35249ca9f71b28f1
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-04-09 05:35:21 -0300

    Add files via upload

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -32,15 +32,15 @@ let crm3 = "SBpbmZvLWRvbmFyLmpz"
 let crm4 = "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz"
 let drm1 = "CkphZGlib3QsIEhlY2hv"
 let drm2 = "IHBvciBAQWlkZW5fTm90TG9naWM"
-let rtx = `*🔰 LoliBot-MD 🔰*\nㅤㅤㅤㅤ*${await tr("Ser sub bot")}*\n\n*${await tr("Con otro telefono que tengas o en la PC escanea este QR para convertirte en un sub bot")}*\n\n*${await tr("1. Haga clic en los tres puntos en la esquina superior derecha")}*\n*${await tr("2. Toca WhatsApp Web")}*\n*${await tr("3. Escanee este codigo QR")}*\n*${await tr("Este código QR expira en 45 segundos!")}*\n\n> *⚠️ ${await tr("No nos hacemos responsable del mal uso que se le pueda dar o si el numero se manda a soporte.. ustedes tienen el deber se seguir al pie de la letra los terminos y condiciones y privacidad (escribe eso y te los dará)")}*`
-let rtx2 = `🟢 *_${await tr("NUEVA FUNCIÓN DE HACERTE UN SUB BOT")}_* 🟢
+let rtx = `*🔰 LoliBot-MD 🔰*\nㅤㅤㅤㅤ*Ser sub bot*\n\n*Con otro telefono que tengas o en la PC escanea este QR para convertirte en un sub bot*\n\n*1. Haga clic en los tres puntos en la esquina superior derecha*\n*2. Toca WhatsApp Web*\n*3. Escanee este codigo QR*\n*Este código QR expira en 45 segundos!*\n\n> *⚠️ No nos hacemos responsable del mal uso que se le pueda dar o si el numero se manda a soporte.. ustedes tienen el deber se seguir al pie de la letra los terminos y condiciones y privacidad (escribe eso y te los dará)*`
+let rtx2 = `🟢 *_NUEVA FUNCIÓN DE HACERTE UN SUB BOT_* 🟢
 
-*1️⃣ ${await tr("Diríjase en los tres puntos en la esquina superior derecha")}*
-*2️⃣ ${await tr("Ir a la opción Dispositivos vinculados")}*
-*3️⃣ ${await tr("da click en vincular con codigo de teléfono")}*
-*4️⃣ ${await tr("pega el codigo a continuación")}*
+*1️⃣ Diríjase en los tres puntos en la esquina superior derecha*
+*2️⃣ Ir a la opción Dispositivos vinculados*
+*3️⃣ da click en vincular con codigo de teléfono*
+*4️⃣ pega el codigo a continuación*
 
-> *⚠️ ${await tr("No nos hacemos responsable del mal uso que se le pueda dar o si el numero se manda a soporte.. ustedes tienen el deber se seguir al pie de la letra los terminos y condiciones y privacidad (escribe eso y te los dará)")}*`
+> *⚠️ No nos hacemos responsable del mal uso que se le pueda dar o si el numero se manda a soporte.. ustedes tienen el deber se seguir al pie de la letra los terminos y condiciones y privacidad (escribe eso y te los dará)*`
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -92,7 +92,7 @@ fs.mkdirSync(pathGataJadiBot, { recursive: true })}
 try {
 args[0] && args[0] != undefined ? fs.writeFileSync(pathCreds, JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
 } catch {
-conn.reply(m.chat, `*⚠️ ${await tr("Use correctamente el comando")}:* \`${usedPrefix + command} code\``, m)
+conn.reply(m.chat, `*⚠️ Use correctamente el comando:* \`${usedPrefix + command} code\``, m)
 return
 }
 
@@ -111,7 +111,7 @@ printQRInTerminal: false,
 auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
 msgRetry,
 msgRetryCache,
-browser: ['Windows', 'Chrome'],
+browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['LoliBot-MD (Sub Bot)', 'Chrome','2.0.0'],
 version: version,
 generateHighQualityLinkPreview: true
 };
@@ -186,7 +186,7 @@ if (connection === 'close') {
 if (reason === 428) {
 if (reconnectAttempts < maxAttempts) {
 const delay = 1000 * Math.pow(2, reconnectAttempts); 
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("La conexión")} (+${path.basename(pathGataJadiBot)}) ${await tr("fue cerrada inesperadamente. Intentando reconectar en")} ${delay / 1000} ${await tr("segundos...")} (${await tr("Intento")} ${reconnectAttempts + 1}/${maxAttempts})\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ La conexión (+${path.basename(pathGataJadiBot)}) fue cerrada inesperadamente. Intentando reconectar en ${delay / 1000} segundos... (Intento ${reconnectAttempts + 1}/${maxAttempts})\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 await sleep(1000);
 reconnectAttempts++;
 await creloadHandler(true).catch(console.error);
@@ -197,32 +197,32 @@ console.log(chalk.redBright(`Sub-bot (+${path.basename(pathGataJadiBot)}) agotó
 await creloadHandler(true).catch(console.error)*/
 }
 if (reason === 408) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("La conexión")} (+${path.basename(pathGataJadiBot)}) ${await tr("se perdió o expiró. Razón")}: ${reason}. ${await tr("Intentando reconectar...")}\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ La conexión (+${path.basename(pathGataJadiBot)}) se perdió o expiró. Razón: ${reason}. Intentando reconectar...\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 await creloadHandler(true).catch(console.error)
 }
 if (reason === 440) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("La conexión")} (+${path.basename(pathGataJadiBot)}) ${await tr("fue reemplazada por otra sesión activa.")}\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
-if (options.fromCommand) m?.chat ? await conn.sendMessage(m.chat, {text : await tr('*⚠️ HEMOS DETECTADO UNA NUEVA SESIÓN, BORRE LA NUEVA SESIÓN PARA CONTINUAR*\n\n> *SI HAY ALGÚN PROBLEMA VUELVA A CONECTARSE*') }, { quoted: m || null }) : ""
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ La conexión (+${path.basename(pathGataJadiBot)}) fue reemplazada por otra sesión activa.\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+if (options.fromCommand) m?.chat ? await conn.sendMessage(m.chat, {text : '*⚠️ HEMOS DETECTADO UNA NUEVA SESIÓN, BORRE LA NUEVA SESIÓN PARA CONTINUAR*\n\n> *SI HAY ALGÚN PROBLEMA VUELVA A CONECTARSE*' }, { quoted: m || null }) : ""
 }
 if (reason == 405 || reason == 401) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("La sesión")} (+${path.basename(pathGataJadiBot)}) ${await tr("fue cerrada. Credenciales no válidas o dispositivo desconectado manualmente.")}\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ La sesión (+${path.basename(pathGataJadiBot)}) fue cerrada. Credenciales no válidas o dispositivo desconectado manualmente.\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 fs.rmdirSync(pathGataJadiBot, { recursive: true })
-if (options.fromCommand) return m?.chat ? await conn.sendMessage(m.chat, {text : await tr('*🟢 REENVIAR NUEVAMENTE EL COMANDO....*') }, { quoted: m || null }) : ''
+if (options.fromCommand) return m?.chat ? await conn.sendMessage(m.chat, {text : '*🟢 REENVIAR NUEVAMENTE EL COMANDO....*' }, { quoted: m || null }) : ''
 }
 if (reason === 500) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("Conexión perdida en la sesión")} (+${path.basename(pathGataJadiBot)}). ${await tr("Borrando datos...")}\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ Conexión perdida en la sesión (+${path.basename(pathGataJadiBot)}). Borrando datos...\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 return creloadHandler(true).catch(console.error)
 if (options.fromCommand) {
-m?.chat ? await conn.sendMessage(m.chat, {text: await tr('🔴 *LA CONEXIÓN SE HA CERRADO, DEBERÁ DE CONECTARSE MANUALMENTE USANDO EL COMANDO #serbot Y REESCANEAR EL NUEVO CÓDIGO QR*') }, { quoted: m || null }) : ""
+m?.chat ? await conn.sendMessage(m.chat, {text: '🔴 *LA CONEXIÓN SE HA CERRADO, DEBERÁ DE CONECTARSE MANUALMENTE USANDO EL COMANDO #serbot Y REESCANEAR EL NUEVO CÓDIGO QR*' }, { quoted: m || null }) : ""
 }
 //fs.rmdirSync(pathGataJadiBot, { recursive: true })
 }
 if (reason === 515) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("Reinicio automático para la sesión")} (+${path.basename(pathGataJadiBot)}).\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ Reinicio automático para la sesión (+${path.basename(pathGataJadiBot)}).\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 await creloadHandler(true).catch(console.error)
 }
 if (reason === 403) {
-console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ ${await tr("Sesión cerrada o cuenta en soporte para la sesión")} (+${path.basename(pathGataJadiBot)}).\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
+console.log(chalk.bold.magentaBright(`\n╭┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡\n┆ Sesión cerrada o cuenta en soporte para la sesión (+${path.basename(pathGataJadiBot)}).\n╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄ • • • ┄┄┄┄┄┄┄┄┄┄┄┄┄┄⟡`))
 fs.rmdirSync(pathGataJadiBot, { recursive: true })
 }}
 
@@ -233,18 +233,18 @@ if (!global.db.data?.users) loadDatabase()
 let userName, userJid 
 userName = sock.authState.creds.me.name || 'Anónimo'
 userJid = sock.authState.creds.me.jid || `${path.basename(pathGataJadiBot)}@s.whatsapp.net`
-console.log(chalk.bold.cyanBright(`\n▣─────────────────────────────···\n│\n│❧ ${userName} (+${path.basename(pathGataJadiBot)}) ${await tr("CONECTADO CORRECTAMENTE", "𝙲𝙾𝙽𝙴𝙲𝚃𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴")} ✅\n│\n▣─────────────────────────────···`))
+console.log(chalk.bold.cyanBright(`\n▣─────────────────────────────···\n│\n│❧ ${userName} (+${path.basename(pathGataJadiBot)}) 𝙲𝙾𝙽𝙴𝙲𝚃𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴✅\n│\n▣─────────────────────────────···`))
 sock.isInit = true
 global.conns.push(sock)
 
 let user = global.db.data?.users[`${path.basename(pathGataJadiBot)}@s.whatsapp.net`]
-m?.chat ? await conn.sendMessage(m.chat, {text : args[0] ? await tr(`*✅ Ya esta conectado!! Por favor espere se esta cargador los mensajes.....*`) : `*${await tr("Conectado exitosamente con WhatsApp")} ✅*\n\n*💻 Bot:* +${path.basename(pathGataJadiBot)}\n*👤 ${await tr("Dueño")}:* ${userName}\n\n${await tr("*Nota: Con la nueva función de auto-reinicio (Beta)*, Si el bot principal se reinicia o se desactiva, los sub-bots se reiniciarán automáticamente, asegurando que sigan activos sin interrupciones.\n\n> *Unirte a nuestro canal para informarte de todas la Actualizaciónes/novedades sobre el bot*")}\n${nna2}`}, { quoted: m }) : ''
-let chtxt = `*${await tr("Se detectó un nuevo Sub-Bot conectado ")} 💻✨*
+m?.chat ? await conn.sendMessage(m.chat, {text : args[0] ? `✅ Ya esta conectado!! Por favor espere se esta cargador los mensajes.....*` : `*Conectado exitosamente con WhatsApp ✅*\n\n*💻 Bot:* +${path.basename(pathGataJadiBot)}\n*👤 Dueño:*  ${userName}\n\n*Nota: Con la nueva función de auto-reinicio (Beta)*, Si el bot principal se reinicia o se desactiva, los sub-bots se reiniciarán automáticamente, asegurando que sigan activos sin interrupciones.\n\n> *Unirte a nuestro canal para informarte de todas la Actualizaciónes/novedades sobre el bot*\n${nna2}`}, { quoted: m }) : ''
+let chtxt = `*Se detectó un nuevo Sub-Bot conectado 💻✨*
 
 *✨ Bot :* wa.me/${path.basename(pathGataJadiBot)}
-*👤 ${await tr("Dueño")} :* ${userName}
-*🔑 ${await tr("Método de conexión")} :* ${mcode ? await tr('Código de 8 dígitos') : await tr('Código QR')}
-*💻 ${await tr("Navegador")} :* ${mcode ? 'Ubuntu' : 'Chrome'}
+*👤 Dueño :* ${userName}
+*🔑 Método de conexión :* ${mcode ? 'Código de 8 dígitos' : 'Código QR'}
+*💻 Navegador :* ${mcode ? 'Ubuntu' : 'Chrome'}
 `.trim()
 let ppch = await sock.profilePictureUrl(userJid, 'image').catch(_ => imageUrl.getRandom())
 await sleep(3000)
@@ -363,46 +363,46 @@ for (const channelId of Object.values(global.ch)) {
 await conn.newsletterFollow(channelId).catch(() => {})
 }}
 
+const activeConnections = new Set()
 async function checkSubBots() {
-    const subBotDir = path.resolve("./jadibts");
-    if (!fs.existsSync(subBotDir)) return;
+    const subBotDir = path.resolve("./jadibts")
+    if (!fs.existsSync(subBotDir)) return
+
     const subBotFolders = fs.readdirSync(subBotDir).filter(folder => 
         fs.statSync(path.join(subBotDir, folder)).isDirectory()
-    );
+    )
 
     for (const folder of subBotFolders) {
-        const pathGataJadiBot = path.join(subBotDir, folder);
-        const credsPath = path.join(pathGataJadiBot, "creds.json");
-        const subBot = global.conns.find(conn => 
-            conn.user?.jid?.includes(folder) || path.basename(pathGataJadiBot) === folder);
+        const pathGataJadiBot = path.join(subBotDir, folder)
+        const credsPath = path.join(pathGataJadiBot, "creds.json")
 
-        if (!fs.existsSync(credsPath)) continue;
+        if (!fs.existsSync(credsPath)) continue
 
-        if (!subBot || !subBot.user) {
-            console.log(chalk.bold.yellowBright(`Sub-bot (+${folder}) no conectado. Intentando activarlo...`));
-            const retries = retryMap.get(folder) || 0;
-            if (retries >= 5) {
-                console.log(chalk.redBright(`Sub-bot (+${folder}) alcanzó límite de reintentos. intentando más tardes`));
-               //fs.rmdirSync(pathGataJadiBot, { recursive: true });
-                retryMap.delete(folder);
-                continue;
-            }
+        const isAlreadyConnected = global.conns.find(conn =>
+            conn.user?.jid?.includes(folder) || path.basename(pathGataJadiBot) === folder
+        )
 
-            try {
-                await gataJadiBot({
-                    pathGataJadiBot,
-                    m: null,
-                    conn: global.conn,
-                    args: [],
-                    usedPrefix: '#',
-                    command: 'jadibot',
-                    fromCommand: false
-                });
-                retryMap.delete(folder); // Resetear intentos si se conecta
-            } catch (e) {
-                console.error(chalk.redBright(`Error al activar sub-bot (+${folder}):`), e);
-                retryMap.set(folder, retries + 1);
-            }
+        if (isAlreadyConnected || activeConnections.has(folder)) {
+            continue
+        }
+
+        console.log(chalk.bold.yellowBright(`Sub-bot (+${folder}) no conectado. Intentando activarlo...`))
+        activeConnections.add(folder)
+
+        try {
+            await gataJadiBot({
+                pathGataJadiBot,
+                m: null,
+                conn: global.conn,
+                args: [],
+                usedPrefix: '#',
+                command: 'jadibot',
+                fromCommand: false
+            })
+        } catch (e) {
+            console.error(chalk.redBright(`Error al activar sub-bot (+${folder}):`), e)
+        } finally {
+            setTimeout(() => activeConnections.delete(folder), 30 * 1000) // Espera 30s antes de permitir otro intento
         }
     }
 }
