051a95ec8d71fc9b1f80a6a703e851b29771efe2
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-07-14 15:19:00 -0300

    Add files via upload

--- plugins/sticker-attp y ttp.js
+++ plugins/sticker-attp y ttp.js
@@ -15,7 +15,7 @@ if (text.length > 30) return m.reply(`⚠️ El texto no puede tener más de 30 
 //let stiker = await sticker(null,`${info.fgmods.url}/maker/attp?text=${teks}&apikey=${info.fgmods.key}`, f, g)
 let res = await fetch(`https://api.neoxr.eu/api/attp?text=${teks}%21&color=%5B%22%23FF0000%22%2C+%22%2300FF00%22%2C+%22%230000FF%22%5D&apikey=GataDios`)
 let json = await res.json()
-if (!json.status) return 
+if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
 let stiker = await sticker(null, json.data.url, f, g)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp}}}, { quoted: m })
 }
@@ -24,7 +24,7 @@ if (command == 'ttp' || command == 'brat') {
 if (text.length > 30) return m.reply(`⚠️ El texto no puede tener más de 30 caracteres.\n\n✍️ Intenta con algo más corto.`)
 let res = await fetch(`https://api.neoxr.eu/api/brat?text=${teks}&apikey=GataDios`)
 let json = await res.json()
-if (!json.status) return m.reply('❌ No se pudo generar la imagen.')
+if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
 let stiker = await sticker(null, json.data.url, f, g)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp}}}, { quoted: m })
 }
@@ -33,7 +33,7 @@ if (command == 'brat2' || command == 'bratvid') {
 if (text.length > 30) return m.reply(`⚠️ El texto no puede tener más de 30 caracteres.\n\n✍️ Intenta con algo más corto.`)
 let res = await fetch(`https://api.neoxr.eu/api/bratvid?text=${teks}&apikey=GataDios`)
 let json = await res.json()
-if (!json.status) return m.reply('❌ No se pudo generar la imagen.')
+if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
 let stiker = await sticker(null, json.data.url, f, g)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply: { showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp }}}, { quoted: m })
 }
