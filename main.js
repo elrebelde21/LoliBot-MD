393b1c12d91af5aac9004672343e7b3bb4cc3e64
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-07-24 23:48:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ console.info = () => {}
 const sock = baileys.makeWASocket({
 printQRInTerminal: !usarCodigo && !fs.existsSync(BOT_CREDS_PATH),
 logger: pino({ level: 'silent' }),   
-browser: ["Ubuntu", "Chrome", "20.0.04"],
+browser: ['Windows', 'Chrome'],
 auth: { creds: state.creds,
 keys: baileys.makeCacheableSignalKeyStore(state.keys, pino({ level: 'silent' }))
 },
