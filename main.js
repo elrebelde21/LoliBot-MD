7ff47d7016f9d9c773f3f601fea64fc32f92d995
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-07-30 11:46:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -206,8 +206,8 @@ const age = now - modifiedTime;
 if (age > 3 * 60 * 1000) {
 fs.unlinkSync(filePath);
 }
-console.log(chalk.gray(`┏━━━━━━⪻♻️ AUTO-CLEAR 🗑️⪼━━━━━━•\n┃→ ARCHIVOS DE LA CARPETA TMP ELIMINADOS\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━•`));
 })
+console.log(chalk.gray(`┏━━━━━━⪻♻️ AUTO-CLEAR 🗑️⪼━━━━━━•\n┃→ ARCHIVOS DE LA CARPETA TMP ELIMINADOS\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━•`));
 } catch (err) {
 console.error('Error cleaning temporary files:', err);
 }}, 30 * 1000);
