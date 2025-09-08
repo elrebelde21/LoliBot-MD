5ab67f4ea0e341d63d91710713b9b10121b2b81e
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-09-08 08:08:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ nn3: "https://chat.whatsapp.com/GXNXKWSEckU1j1c7sItTxK", //Colab Loli & Gata
 nn4: "https://chat.whatsapp.com/Em4Byf4w5VgHObPvZQlfnM", //Enlace LoliBot
 nn5: "https://chat.whatsapp.com/J5hQvECqR4W6uTT84uiSBx", //A.T.M.M
 nn6: "https://chat.whatsapp.com/ILAHJeZsHh973tQ96i2aqS", //Dev support 
-nna: "https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A",
+nna: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z",
 nna2: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z"
 }
 
