f3892d8a38e87c1978009c50238d812bdd34d022
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2026-01-28 20:23:44 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ nn3: "https://chat.whatsapp.com/GXNXKWSEckU1j1c7sItTxK", //Colab Loli & Gata
 nn4: "https://chat.whatsapp.com/Em4Byf4w5VgHObPvZQlfnM", //Enlace LoliBot
 nn5: "https://chat.whatsapp.com/J5hQvECqR4W6uTT84uiSBx", //A.T.M.M
 nn6: "https://chat.whatsapp.com/ILAHJeZsHh973tQ96i2aqS", //Dev support 
-nna: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z",
-nna2: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z"
+nna: "https://whatsapp.com/channel/0029VagJ2FF4CrfrS8BoLW2b",
+nna2: "https://whatsapp.com/channel/0029VasFtcGC1FuA6FiE5R0B"
 }
 
 //----------------------------------------------------
