c53a672170b5e8ddf01495b927f7c51989202054
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2026-04-25 19:48:46 -0300

    Add files via upload

--- plugins/sticker-attp y ttp.js
+++ plugins/sticker-attp y ttp.js
@@ -12,30 +12,94 @@ conn.fakeReply(m.chat, `Calma crack estoy haciendo tu texto a sticker 👏\n\n> 
 
 if (command == 'attp') {
 if (text.length > 40) return m.reply(`⚠️ El texto no puede tener más de 40 caracteres.\n\n✍️ Intenta con algo más corto.`)
-//let stiker = await sticker(null,`${info.fgmods.url}/maker/attp?text=${teks}&apikey=${info.fgmods.key}`, f, g)
-let res = await fetch(`https://api.neoxr.eu/api/attp?text=${teks}%21&color=%5B%22%23FF0000%22%2C+%22%2300FF00%22%2C+%22%230000FF%22%5D&apikey=${info.neoxr.key}`)
-let json = await res.json()
-if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
-let stiker = await sticker(null, json.data.url, f, g)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp}}}, { quoted: m })
+const teks = encodeURIComponent(text)
+const url = `https://api.delirius.store/canvas/attp?text=${teks}`
+let stiker = await sticker(null, url, f, g)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: null, thumbnail: m.pp}}}, { quoted: m })
 }
 
 if (command == 'ttp' || command == 'brat') {
-if (text.length > 300) return m.reply(`⚠️ El texto no puede tener más de 300 caracteres.\n\n✍️ Intenta con algo más corto.`)
-let res = await fetch(`https://api.neoxr.eu/api/brat?text=${teks}&apikey=${info.neoxr.key}`)
+if (!text) return m.reply(`✍️ Escribe un texto.`)
+if (text.length > 300) return m.reply(`⚠️ El texto no puede tener más de 300 caracteres.`)
+const teks = encodeURIComponent(text)
+try {
+const url = `https://api.delirius.store/canvas/brat?text=${teks}`
+let stiker = await sticker(null, url, f, g)
+await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { 
+contextInfo: { 
+forwardingScore: 200,
+isForwarded: false,
+externalAdReply: {
+showAdAttribution: false,
+title: info.wm,
+body: info.vs,
+mediaType: 2,
+sourceUrl: null,
+thumbnail: m.pp
+}}})
+} catch {
+try {
+let res = await fetch(`https://api.mitzuki.xyz/maker/brat?size=512&text=${teks}&apikey=elrebelde21`)
 let json = await res.json()
-if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
-let stiker = await sticker(null, json.data.url, f, g)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp}}}, { quoted: m })
+if (!json.status) return m.reply('Ufff la puta api se cayó 😒 intenta más tarde')
+let stiker = await sticker(null, json.data.image_url, f, g)
+await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, {
+contextInfo: {
+forwardingScore: 200,
+isForwarded: false,
+externalAdReply: {
+showAdAttribution: false,
+title: info.wm,
+body: info.vs,
+mediaType: 2,
+sourceUrl: null,
+thumbnail: m.pp
+}}})
+} catch (e) {
+return m.reply('❌ Error creando sticker brat.')
+}}
 }
+
 
 if (command == 'brat2' || command == 'bratvid') {
 if (text.length > 250) return m.reply(`⚠️ El texto no puede tener más de 250 caracteres.\n\n✍️ Intenta con algo más corto.`)
-let res = await fetch(`https://api.neoxr.eu/api/bratvid?text=${teks}&apikey=${info.neoxr.key}`)
+const teks = encodeURIComponent(text)
+try {
+const url = `https://api.delirius.store/canvas/bratvideo?text=${teks}`
+let stiker = await sticker(null, url, f, g)
+await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { 
+contextInfo: { 
+forwardingScore: 200,
+isForwarded: false,
+externalAdReply: {
+showAdAttribution: false,
+title: info.wm,
+body: info.vs,
+mediaType: 2,
+sourceUrl: null,
+thumbnail: m.pp
+}}})
+} catch {
+try {
+let res = await fetch(`https://api.mitzuki.xyz/maker/brat?size=512&text=${teks}&apikey=elrebelde21`)
 let json = await res.json()
-if (!json.status) return m.reply('Ufff la puta api se cayo 😒 pura mamada vuelve intentarlo mas tarde')
-let stiker = await sticker(null, json.data.url, f, g)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply: { showAdAttribution: false, title: info.wm, body: info.vs, mediaType: 2, sourceUrl: info.md, thumbnail: m.pp }}}, { quoted: m })
+if (!json.status) return m.reply('Ufff la puta api se cayó 😒 intenta más tarde')
+let stiker = await sticker(null, json.data.image_url, f, g)
+await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, {
+contextInfo: {
+forwardingScore: 200,
+isForwarded: false,
+externalAdReply: {
+showAdAttribution: false,
+title: info.wm,
+body: info.vs,
+mediaType: 2,
+sourceUrl: null,
+thumbnail: m.pp
+}}})
+} catch (e) {
+return m.reply('❌ Error creando sticker brat.')
+}}
 }
 }
 handler.help = ['attp', 'brat', 'bratvid'];
